a7eceb99b4a617dd57224f313a0c2940544faac8
prateekCodeLab <prateekkmr3151@gmail.com>
2025-12-19 21:04:30 +0530

    chore: rename app from KhojBin to CityCleaner

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:CityCleaner/main.dart';
+import 'package:citycleaner/main.dart';
 
 void main() {
   testWidgets('App loads without crash', (WidgetTester tester) async {
